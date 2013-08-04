7e3ace389cead8a81c13fb72f79a8c53e8548b37
Peter de Lange <peter.de.lange@rwth-aachen.de>
2013-08-04 18:27:18 +0200

    Added config-file values, prepared storage facilities

--- java/i5/las2peer/services/monitoring/processing/MonitoringDataProcessingService.java
+++ java/i5/las2peer/services/monitoring/processing/MonitoringDataProcessingService.java
@@ -1,12 +1,16 @@
 package i5.las2peer.services.monitoring.processing;
 
 import i5.las2peer.api.Service;
+import i5.las2peer.logging.NodeObserver.Event;
 import i5.las2peer.logging.monitoring.MonitoringMessage;
 import i5.las2peer.security.Agent;
 import i5.las2peer.security.AgentException;
 import i5.las2peer.security.L2pSecurityException;
 import i5.las2peer.security.MonitoringAgent;
 import i5.las2peer.tools.CryptoException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 
@@ -24,12 +28,30 @@ import i5.las2peer.tools.CryptoException;
 public class MonitoringDataProcessingService extends Service{
 	private static final String AGENT_PASS = "ProcessingAgentPass"; //The pass phrase for the receivingAgent
 	private MonitoringAgent receivingAgent; //This agent will be responsible for receiving all incoming message
+	private Map<Long, String> monitoredServices = new HashMap<Long, String>(); 
+	
+	private String databaseName = "to be done";
+	private	String databaseType = "to be done";
+	private String databaseHost = "to be done";
+	private int databasePort = 123;
+	private String databaseUser = "to be done";
+	private String databasePassword = "to be done";
+	
 	
 	public MonitoringDataProcessingService(){
 		setFieldValues(); //This sets the values of the property file
+		//TODO: Setup database here
 	}
 	
 	
+	/**
+	 * 
+	 * Will be called by the receiving {@link i5.las2peer.security.MonitoringAgent} of this service,
+	 * if it receives a message from a monitored node.
+	 * 
+	 * @param messages an array of {@link i5.las2peer.logging.monitoring.MonitoringMessage}s
+	 * 
+	 */
 	public boolean getMessages(MonitoringMessage[] messages){
 		Agent requestingAgent = getActiveAgent();
 		if(receivingAgent == null){
@@ -40,14 +62,56 @@ public class MonitoringDataProcessingService extends Service{
 			System.out.println("Monitoring: I only take messages from my own Agent!");
 			return false;
 		}
-		System.out.println("Got something, need a database!"); //TODO
+		System.out.println("Monitoring: Got a monitoring message!");
+		return processMessages(messages);
+	}
+	
+	
+	private boolean processMessages(MonitoringMessage[] messages) {
+		boolean returnStatement = true;
+		for(MonitoringMessage message : messages){
+			if(message == null) //Happens when the node has sent its last messages
+				return returnStatement;
+			if(message.getEvent() == Event.SERVICE_ADD_TO_MONITORING){
+				monitoredServices.put(message.getSourceAgentId(), message.getRemarks());
+			}
+			else if(Math.abs(message.getEvent().getCode()) >= 7000 && (Math.abs(message.getEvent().getCode()) < 8000)){ //Service Messages
+				if(monitoredServices.containsKey(message.getSourceAgentId())){
+					returnStatement = persistMessage(message);
+					if(!returnStatement)
+						return returnStatement;
+				}
+			}
+			else{
+				returnStatement = persistMessage(message);
+				if(!returnStatement)
+					return returnStatement;
+			}
+		}
+		return returnStatement;
+	}
+	
+	
+	private boolean persistMessage(MonitoringMessage message) {
+		// TODO store Message
+		System.out.println("Monitoring: Persisting message with type " + message.getEvent());
 		return true;
 	}
 	
 	
+	/**
+	 * 
+	 * Returns the id of this monitoring agent (that will be responsible for message receiving).
+	 * Creates one if not existent.
+	 * 
+	 * @param greetings will be printed in the console and is only used to control registering
+	 * 
+	 * @return the id
+	 * 
+	 */
 	public long getReceivingAgentId(String greetings){
+		System.out.println("Monitoring: Service requests receiving agent id: " + greetings);
 		if(receivingAgent == null){
-			System.out.println("Message from invokation: " + greetings);
 			try {
 				receivingAgent = MonitoringAgent.createMonitoringAgent(AGENT_PASS);
 				receivingAgent.unlockPrivateKey(AGENT_PASS);
@@ -57,7 +121,6 @@ public class MonitoringDataProcessingService extends Service{
 				e.printStackTrace();
 			}
 		}
-		
 		return this.receivingAgent.getId();
 	}
 }
---
+++ start_scripts/start_network_and start_processing_service.bat
@@ -0,0 +1,6 @@
+cd ..
+set BASE=%CD%
+set CLASSPATH="%BASE%/lib/*;"
+
+java -cp %CLASSPATH% i5.las2peer.testing.L2pNodeLauncher -s 9010 - uploadStartupDirectory startService('i5.las2peer.services.monitoring.processing.MonitoringDataProcessingService','MDPSPass') interactive
+pause
