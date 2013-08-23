baf619485894a67c56805e40bad8089b37d29f99
Peter de Lange <peter.de.lange@rwth-aachen.de>
2013-08-23 22:48:07 +0200

    Insertion works except for the "on duplicate" cases (Todo..)

--- java/i5/las2peer/services/monitoring/processing/database/DatabaseInsertStatement.java
+++ java/i5/las2peer/services/monitoring/processing/database/DatabaseInsertStatement.java
@@ -61,7 +61,29 @@ public class DatabaseInsertStatement {
 		}
 		
 		else if(databaseType == SQLDatabaseType.DB2){
-			throw new Exception("Not implemented yet!");
+			if(table.equals("MESSAGE")){
+				return returnDB2MessageStatement(monitoringMessage, DB2Schema);
+			}
+			
+			else if(table.equals("AGENT")){
+				return returnDB2AgentStatement(monitoringMessage, DB2Schema);
+			}
+			
+			else if(table.equals("SERVICE")){
+				return returnDB2ServiceStatement(monitoringMessage, DB2Schema);
+			}
+			
+			else if(table.equals("NODE")){
+				return returnDB2NodeStatement(monitoringMessage, DB2Schema);
+			}
+			
+			else if(table.equals("REGISTERED_AT")){
+				return returnDB2RegisteredAtStatement(monitoringMessage, DB2Schema);				
+			}
+			
+			else{
+				throw new Exception("Don't know table!");
+			}
 		}
 		
 		else{
@@ -236,11 +258,11 @@ public class DatabaseInsertStatement {
 	
 	/**
 	 * 
-	 * Returns a SQL statement for the Node table.
+	 * Returns a MySQL statement for the Node table.
 	 * 
 	 * @param monitoringMessage a {@link i5.las2peer.logging.monitoring.MonitoringMessage} that contains the information to be stored
 	 * 
-	 * @return a SQL statement
+	 * @return a MySQL statement
 	 * 
 	 * @throws Exception if the given information was not correct or sufficient for the desired table entry
 	 * 
@@ -267,7 +289,6 @@ public class DatabaseInsertStatement {
 			String nodeLocation = monitoringMessage.getRemarks().substring(nodeLocationStart, nodeLocationEnd);
 			returnStatement = "INSERT INTO NODE(NODE_ID, NODE_LOCATION) VALUES(";
 			//Duplicate can happen because of new node notices
-
 			returnStatement += "'" + nodeId +"', '" + nodeLocation + "') ON DUPLICATE KEY UPDATE NODE_ID = NODE_ID;";
 			return returnStatement;
 		}
@@ -276,5 +297,214 @@ public class DatabaseInsertStatement {
 		}
 	}
 	
+	
+	/**
+	 * 
+	 * Returns a DB2 statement for the message table.
+	 * 
+	 * @param monitoringMessage a {@link i5.las2peer.logging.monitoring.MonitoringMessage} that contains the information to be stored
+	 * @param DB2Schema the database schema
+	 * 
+	 * @return a DB2 statement
+	 * 
+	 */
+	private static String returnDB2MessageStatement(MonitoringMessage monitoringMessage, String DB2Schema){
+		String returnStatement;
+		
+		String event =  "'" + monitoringMessage.getEvent().toString() + "'";
+		String timestamp = ", " + monitoringMessage.getTimestamp();
+		String sourceNode = "";
+		String sourceAgentId = "";
+		String destinationNode = "";
+		String destinationAgentId = "";
+		String remarks = "";
+		returnStatement = "INSERT INTO " + DB2Schema + ".MESSAGE (EVENT, TIME_STAMP";
+		if(monitoringMessage.getSourceNode() != null){
+			returnStatement += ", SOURCE_NODE";
+			sourceNode = ", '" + monitoringMessage.getSourceNode().substring(0, 12) + "'";
+		}
+		if(monitoringMessage.getSourceAgentId() != null){
+			returnStatement += ", SOURCE_AGENT";
+			sourceAgentId = ", " + monitoringMessage.getSourceAgentId().toString();
+		}
+		if(monitoringMessage.getDestinationNode() != null){
+			returnStatement += ", DESTINATION_NODE";
+			destinationNode = ", '" + monitoringMessage.getDestinationNode().substring(0, 12) + "'";
+		}
+		if(monitoringMessage.getDestinationAgentId() != null){
+			returnStatement += ", DESTINATION_AGENT";
+			destinationAgentId = ", " + monitoringMessage.getDestinationAgentId().toString();
+
+		}
+		if(monitoringMessage.getRemarks() != null){
+			returnStatement += ", REMARKS";
+			remarks = ", '" + monitoringMessage.getRemarks() + "'";
+		}
+		returnStatement += ") VALUES(";
+		returnStatement += event + timestamp + sourceNode + sourceAgentId + destinationNode + destinationAgentId + remarks;
+		returnStatement += ")";
+		return returnStatement;
+	}
+	
+	
+	/**
+	 * 
+	 * Returns a DB2 statement for the Agent table.
+	 * 
+	 * @param monitoringMessage a {@link i5.las2peer.logging.monitoring.MonitoringMessage} that contains the information to be stored
+	 * @param DB2Schema the database schema
+	 * 
+	 * @return a DB2 statement
+	 * 
+	 * @throws Exception if the given information was not correct or sufficient for the desired table entry
+	 * 
+	 */
+	private static String returnDB2AgentStatement(MonitoringMessage monitoringMessage, String DB2Schema) throws Exception{
+		String returnStatement;
+		
+		if(monitoringMessage.getSourceNode() == null || monitoringMessage.getSourceAgentId() == null || monitoringMessage.getRemarks() == null)
+			throw new Exception("Missing information for persisting agent entity!");
+		
+		String agentType = null;
+		if(monitoringMessage.getEvent() == Event.SERVICE_ADD_TO_MONITORING)
+			agentType = "SERVICE";
+		else if(monitoringMessage.getEvent() == Event.AGENT_REGISTERED){
+			if(monitoringMessage.getRemarks().equals("ServiceAgent")){
+				throw new Exception("ServiceAgents are only persisted when added to monitoring!");
+			}
+			else if(monitoringMessage.getRemarks().equals("UserAgent")){
+				agentType = "USER";
+			}
+			else if(monitoringMessage.getRemarks().equals("GroupAgent")){
+				agentType = "GROUP";
+			}
+			else if(monitoringMessage.getRemarks().equals("MonitoringAgent")){
+				agentType = "MONITORING";
+			}
+			else if(monitoringMessage.getRemarks().equals("Mediator")){
+			//Thats right, we treat mediators as agents (as from a monitoring point of view, this is the same)
+				agentType = "MEDIATOR";
+			}
+			else{
+				throw new Exception("Unknown remarks entry for persisting agent entity: " + monitoringMessage.getRemarks());
+			}
+		}
+		else{
+			throw new Exception("Agent entities will only be persisted if registered at a node!"); 
+		}
+		returnStatement = "INSERT INTO " + DB2Schema + ".AGENT(AGENT_ID, TYPE) VALUES(";
+		returnStatement += monitoringMessage.getSourceAgentId() + ", '" + agentType + "')";
+		return returnStatement;
+	}
+	
+	
+	/**
+	 * 
+	 * Returns a DB2 statement for the Service table.
+	 * 
+	 * @param monitoringMessage a {@link i5.las2peer.logging.monitoring.MonitoringMessage} that contains the information to be stored
+	 * @param DB2Schema the database schema
+	 * 
+	 * @return a DB2 statement
+	 * 
+	 * @throws Exception if the given information was not correct or sufficient for the desired table entry
+	 * 
+	 */
+	private static String returnDB2ServiceStatement(MonitoringMessage monitoringMessage, String DB2Schema) throws Exception {
+		String returnStatement;
+		
+		if(monitoringMessage.getSourceAgentId() == null || monitoringMessage.getRemarks() == null)
+			throw new Exception("Missing information for persisting service entity!");
+		
+		returnStatement = "INSERT INTO " + DB2Schema + ".SERVICE(AGENT_ID, SERVICE_CLASS_NAME) VALUES(";
+		returnStatement += monitoringMessage.getSourceAgentId() + ", '" + monitoringMessage.getRemarks() +"')";
+		return returnStatement;
+	}
+	
+	
+	/**
+	 * 
+	 * Returns a DB2 statement for the "Registered At" table.
+	 * 
+	 * @param monitoringMessage a {@link i5.las2peer.logging.monitoring.MonitoringMessage} that contains the information to be stored
+	 * @param DB2Schema the database schema
+	 * 
+	 * @return a DB2 statement
+	 * 
+	 * @throws Exception if the given information was not correct or sufficient for the desired table entry
+	 * 
+	 */
+	private static String returnDB2RegisteredAtStatement(MonitoringMessage monitoringMessage, String DB2Schema) throws Exception {
+		String returnStatement;
+		if(monitoringMessage.getTimestamp() == null || monitoringMessage.getSourceNode() == null)
+			throw new Exception("Missing information for 'registered at' entity!");
+	
+		String timestamp = "'" + new Timestamp(monitoringMessage.getTimestamp()).toString() + "'";
+		String nodeId = monitoringMessage.getSourceNode().substring(0, 12);
+	
+		if(monitoringMessage.getEvent() == Event.AGENT_REGISTERED || monitoringMessage.getEvent() == Event.SERVICE_ADD_TO_MONITORING){
+			if(monitoringMessage.getSourceAgentId() == null)
+				throw new Exception("Missing information for persisting 'registered at' entity!");
+			returnStatement = "INSERT INTO " + DB2Schema + ".REGISTERED_AT(REGISTRATION_DATE, AGENT_ID, RUNNING_AT) VALUES(";
+			returnStatement += timestamp + ", " + monitoringMessage.getSourceAgentId() + ", '" + nodeId +"')";
+			return returnStatement;
+		}
+		else if(monitoringMessage.getEvent() == Event.AGENT_REMOVED || monitoringMessage.getEvent() == Event.SERVICE_SHUTDOWN){
+			returnStatement = "UPDATE " + DB2Schema + ".REGISTERED_AT SET UNREGISTRATION_DATE=";
+			returnStatement += timestamp;
+			returnStatement += " WHERE RUNNING_AT='" + nodeId + "' AND AGENT_ID=" + monitoringMessage.getSourceAgentId() + " AND UNREGISTRATION_DATE IS NULL";
+			return returnStatement;
+		}
+		else{ //We need to unregister those who have not yet unregistered -> update statement!
+			returnStatement = "UPDATE " + DB2Schema + ".REGISTERED_AT SET UNREGISTRATION_DATE=";
+			returnStatement += timestamp;
+			returnStatement += " WHERE RUNNING_AT='" + nodeId + "' AND UNREGISTRATION_DATE IS NULL";
+			return returnStatement;
+		}
+		
+	}
+	
+	
+	/**
+	 * 
+	 * Returns a DB2 statement for the Node table.
+	 * 
+	 * @param monitoringMessage a {@link i5.las2peer.logging.monitoring.MonitoringMessage} that contains the information to be stored
+	 * @param DB2Schema the database schema
+	 * 
+	 * @return a DB2 statement
+	 * 
+	 * @throws Exception if the given information was not correct or sufficient for the desired table entry
+	 * 
+	 */
+	private static String returnDB2NodeStatement(MonitoringMessage monitoringMessage, String DB2Schema) throws Exception {
+		String returnStatement;
+		if(monitoringMessage.getEvent() == Event.NODE_STATUS_CHANGE){
+			if(monitoringMessage.getSourceNode() == null)
+				throw new Exception("Missing information for persisting node entity!");
+			String nodeId = monitoringMessage.getSourceNode().substring(0, 12);
+			int startingLocationPosition = monitoringMessage.getSourceNode().lastIndexOf("/") + 1;
+			String nodeLocation = monitoringMessage.getSourceNode().substring(startingLocationPosition);
+			returnStatement = "INSERT INTO " + DB2Schema + ".NODE(NODE_ID, NODE_LOCATION) VALUES(";
+			returnStatement += "'" + nodeId +"', '" + nodeLocation + "')";
+			return returnStatement;
+		}
+		else if(monitoringMessage.getEvent() == Event.NEW_NODE_NOTICE){
+			if(monitoringMessage.getRemarks() == null)
+				throw new Exception("Missing information for persisting node entity!");
+			int nodeIdStart = monitoringMessage.getRemarks().indexOf("<");
+			int nodeLocationStart = monitoringMessage.getRemarks().lastIndexOf("/") + 1;
+			int nodeLocationEnd = monitoringMessage.getRemarks().indexOf("]");
+			String nodeId = monitoringMessage.getRemarks().substring(nodeIdStart, nodeIdStart+12);
+			String nodeLocation = monitoringMessage.getRemarks().substring(nodeLocationStart, nodeLocationEnd);
+			returnStatement = "INSERT INTO " + DB2Schema + ".NODE(NODE_ID, NODE_LOCATION) VALUES(";
+			//Duplicate can happen because of new node notices
+			returnStatement += "'" + nodeId +"', '" + nodeLocation + "')";
+			return returnStatement;
+		}
+		else{
+			throw new Exception("Node persistence only at new node notice or node creation events!");
+		}
+	}
 	
 }
