eb420ac193ea99701f9eed9a527633340c189506
FBasels <frederik.basels@rwth-aachen.de>
2021-10-26 13:44:56 +0200

    changed check of xAPI forwarding to bots

--- src/main/i5/las2peer/services/mobsos/dataProcessing/MobSOSDataProcessingService.java
+++ src/main/i5/las2peer/services/mobsos/dataProcessing/MobSOSDataProcessingService.java
@@ -309,8 +309,12 @@ public class MobSOSDataProcessingService extends Service {
 				Context.get().invoke("i5.las2peer.services.learningLockerService.LearningLockerService",
 						"sendXAPIstatement", (Serializable) xAPIstatements);
 				
-				if (monitoredServices.values().stream().anyMatch(s ->
-						s.contains("i5.las2peer.services.socialBotManagerService.SocialBotManagerService"))) {
+//				if (monitoredServices.values().stream().anyMatch(s ->
+//						s.contains("i5.las2peer.services.socialBotManagerService.SocialBotManagerService"))) {
+//					Context.getCurrent().invoke("i5.las2peer.services.socialBotManagerService.SocialBotManagerService",
+//							"getXapiStatements", (Serializable) xAPIstatements);
+//				}
+				if (hasBot()) {
 					Context.getCurrent().invoke("i5.las2peer.services.socialBotManagerService.SocialBotManagerService",
 							"getXapiStatements", (Serializable) xAPIstatements);
 				}
